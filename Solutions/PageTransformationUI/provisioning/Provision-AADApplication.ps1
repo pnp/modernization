27a68f18abaa93377bbe852d894b0bf4ba8b1ef5
paolo@pialorsi.com <paolo@pialorsi.com>
2018-12-07 16:23:21 +0100

    Updated scripts, still missing a technique to make the automatic permissions grant

--- Solutions/PageTransformationUI/provisioning/Provision-AADApplication.ps1
+++ Solutions/PageTransformationUI/provisioning/Provision-AADApplication.ps1
@@ -2,6 +2,10 @@
     [Parameter(Mandatory=$true, Position=0)]
     [String] $AppName,
     [Parameter(Mandatory=$true, Position=1)]
+    [String] $HomePageUrl,
+    [Parameter(Mandatory=$true, Position=2)]
+    [String] $ReplyUrl,
+    [Parameter(Mandatory=$true, Position=3)]
     [String] $AppTitle    
 )
 
@@ -16,7 +20,7 @@ if(!$aadPreviwModule)
 function GeneratePasswordCredential() {
     $keyId = (New-Guid).ToString();
     $startDate = Get-Date
-    $endDate = $startDate.AddYears(2)
+    $endDate = $startDate.AddYears(10)
     $random = [System.Security.Cryptography.RandomNumberGenerator]::Create()
     [byte[]]$bytes = New-Object byte[] 32
     $random.GetBytes($bytes)
@@ -37,7 +41,7 @@ function GenerateRequiredResourceAccess() {
     $siteCollectionsFullControlDelegatedPermission.Type = "Scope"
     $siteCollectionsFullControlDelegated.ResourceAccess.Add($siteCollectionsFullControlDelegatedPermission)
         
-        # Sign In and Read User Profile Delegated permission request
+    # Sign In and Read User Profile Delegated permission request
     $signInReadProfileDelegated = New-Object Microsoft.Open.AzureAD.Model.RequiredResourceAccess
     $signInReadProfileDelegated.ResourceAppId = "00000002-0000-0000-c000-000000000000"
     $signInReadProfileDelegated.ResourceAccess = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.ResourceAccess]
@@ -52,34 +56,112 @@ function GenerateRequiredResourceAccess() {
     return $requiredResourcesAccess
 }
 
-Write-Host "Please provide the credential to access the AAD tenant under the cover of your Office 365 target tenant"
+Function GrantOAuthPermission(
+    $clientId,
+    $clientSecret,
+    $permissionResourceId,
+    $permissionScope) {
+
+    $resource = "https://graph.windows.net/"
+    $authority = "https://login.microsoftonline.com/common"
+    $tokenEndpointUri = "$authority/oauth2/token"
+    $client_secret = [System.Web.HttpUtility]::UrlEncode($clientSecret)
+    $content = "grant_type=client_credentials&client_id=$clientId&client_secret=$client_Secret&resource=$resource"
+    
+    Write-Host $tokenEndpointUri
+    Write-Host $content
+
+    $Stoploop = $false
+    [int]$Retrycount = "0"
+    
+    do {
+        try {
+            $response = Invoke-RestMethod -Uri $tokenEndpointUri -Body $content -Method Post -UseBasicParsing
+            Write-Host "Retrieved Access Token for Azure AD Graph API" -ForegroundColor Yellow
+
+            # Assign access token
+            $accessToken = $response.access_token
+    
+            $headers = @{
+                Authorization = "Bearer $accessToken"
+            }
+    
+            $consentBody = @{
+                clientId    = $clientId
+                consentType = "AllPrincipals"
+                startTime   = ((get-date).AddDays(-1)).ToString("yyyy-MM-dd")
+                principalId = $null
+                resourceId  = $permissionResourceId
+                scope       = $permissionScope
+                expiryTime  = ((get-date).AddYears(99)).ToString("yyyy-MM-dd")
+            }
+    
+            $consentBody = $consentBody | ConvertTo-Json
+    
+            Write-Host "Granting permission scope $permissionScope for resource $permissionResourceId" -ForegroundColor White
+            $body = Invoke-RestMethod -Uri "https://graph.windows.net/myorganization/oauth2PermissionGrants?api-version=1.6" -Body $consentBody -Method POST -Headers $headers -ContentType "application/json"
+            Write-Host "Granted permission scope $permissionScope for resource $permissionResourceId" -ForegroundColor Green
+    
+            $Stoploop = $true
+        }
+        catch {
+            if ($Retrycount -gt 5) {
+                Write-Host "Could not get create OAuth2PermissionGrants after 6 retries." -ForegroundColor Red
+                $Stoploop = $true
+            }
+            else {
+                Write-Host "Could not create OAuth2PermissionGrants yet. Retrying in 5 seconds..." -ForegroundColor DarkYellow
+                Start-Sleep -Seconds 5
+                $Retrycount ++
+            }
+        }
+    }
+    While ($Stoploop -eq $false)
+}
+
+Write-Host "Please provide the credential to access the AAD tenant under the cover of your Office 365 target tenant" -ForegroundColor White
 
 # Connect to Azure AD
 $credential = Get-Credential
 Connect-AzureAd -Credential $credential
 
 # Infrastructural variables
-$homePage = "https://$AppName.azurewebsites.net/"
 $identifierURI = "https://sharepointpnp.com/Modernization/Framework"
-$logoutURI = "http://portal.office.com"
 $passwordCredentials = GeneratePasswordCredential
 $requiredResourceAccess = GenerateRequiredResourceAccess
 
 # Check if the application already exists
 $application = Get-AzureADApplication -Filter "identifierUris/any(uri:uri eq '$identifierURI')"
 if ($application) {
-    Write-Warning 'Application already registered in the target tenant! You can delete it and create it again.'
+    Write-Warning 'Application already registered in the target tenant! You should delete it first.'
+
+    return $null
 }        
 else {
     $application = New-AzureADApplication `
         -DisplayName $AppTitle `
         -AvailableToOtherTenants $false `
-        -Homepage $homePage `
-        -ReplyUrls $homePage `
+        -Homepage $HomePageUrl `
+        -ReplyUrls $ReplyUrl `
         -IdentifierUris $identifierURI `
-        -LogoutUrl $logoutURI `
         -RequiredResourceAccess $requiredResourceAccess `
         -PasswordCredentials $passwordCredentials
-}
 
-return @{ Application = $application; ClientId = $application.AppId; ClientSecret = $passwordCredentials.Value }
+    # Creating the Service Principal for the application
+    $servicePrincipal = New-AzureADServicePrincipal -AppId $application.AppId
+
+    # Wait 10 seconds
+    # Start-Sleep -Seconds 10
+
+    # Grant permissions
+    # foreach ($permission in $requiredResourceAccess)
+    # {
+    #     GrantOAuthPermission `
+    #         -clientId $application.AppId `
+    #         -clientSecret $passwordCredentials.Value `
+    #         -permissionResourceId $permission.ResourceAccess[0].Id `
+    #         -permissionScope $permission.ResourceAccess[0].Type        
+    # }
+
+    return @{ Application = $application; ClientId = $application.AppId; ClientSecret = $passwordCredentials.Value }
+}
--- Solutions/PageTransformationUI/provisioning/Provision-ModernizationFramework.ps1
+++ Solutions/PageTransformationUI/provisioning/Provision-ModernizationFramework.ps1
@@ -25,132 +25,159 @@
  $AllowedTenants
 )
 
+$aadAppHomePageUrl = "https://$FunctionAppName.azurewebsites.net"
+$aadAppReplyUrl = "$aadAppHomePageUrl/.auth/login/aad/callback"
+
+Write-Host "Creating the AAD application in the target Office 365 Tenant" -ForegroundColor White
+
 # Register the Azure AD Application and get back ClientId and ClientSecret
-$aadApp = .\Provision-AADApplication.ps1 -AppName $AppName -AppTitle $AppTitle
+$aadApp = .\Provision-AADApplication.ps1 -AppName $AppName -HomePageUrl $aadAppHomePageUrl -ReplyUrl $aadAppReplyUrl -AppTitle $AppTitle
 
-# Install AzureRM command lets, if they are missing
-$azureRMModule = Import-Module AzureRM -ErrorAction SilentlyContinue -PassThru
-if(!$azureRMModule)
+if ($aadApp -eq $null) 
 {
-    Write-Output "Installing AzureADPreview module"
-    Install-Module AzureRM -Force
+    Write-Host "Failed to create the AAD application!" -ForegroundColor Red
 }
-
-# Login to AzureRM
-Login-AzureRmAccount 
-
-# Select the target Subscription
-$subs = Get-AzureRmSubscription
-$sub = $subs | where { $_.Name -eq $SubscriptionName }
-Select-AzureRmSubscription -TenantId $sub.TenantId -SubscriptionId $sub.SubscriptionId
-
-# Create the Resource Group if it does not exist
-$resourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
-if(!$resourceGroup)
+else
 {
-    Write-Host "Resource group '$ResourceGroupName' does not exist."
-    if(!$ResourceGroupLocation) {
-        Write-Host "To create a new resource group, please enter a location.";
-        $ResourceGroupLocation = Read-Host "ResourceGroupLocation";
+    Write-Host "Created the AAD application in the target Office 365 Tenant" -ForegroundColor Green
+    Write-Host ("Client ID: " + $aadApp.ClientId) -ForegroundColor White
+    Write-Host ("Client Secret: " + $aadApp.ClientSecret) -ForegroundColor White
+
+    # Install AzureRM command lets, if they are missing
+    $azureRMModule = Import-Module AzureRM -ErrorAction SilentlyContinue -PassThru
+    if(!$azureRMModule)
+    {
+        Write-Output "Installing AzureADPreview module"
+        Install-Module AzureRM -Force
     }
-    Write-Host "Creating resource group '$ResourceGroupName' in location '$ResourceGroupLocation'";
-    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
-}
-else{
-    Write-Host "Using existing resource group '$ResourceGroupName'";
-}
 
-# Create the Storage Account if it does not exist
-$storageAccount = Get-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
-if(!$storageAccount)
-{
-    Write-Host "Storage account '$StorageAccountName' does not exist."
-    if(!$ResourceGroupLocation) {
-        Write-Host "To create a new storage account, please enter a location.";
-        $ResourceGroupLocation = Read-Host "ResourceGroupLocation";
+    # Login to AzureRM
+    Login-AzureRmAccount 
+
+    # Select the target Subscription
+    $subs = Get-AzureRmSubscription
+    $sub = $subs | where { $_.Name -eq $SubscriptionName }
+    Select-AzureRmSubscription -TenantId $sub.TenantId -SubscriptionId $sub.SubscriptionId
+
+    # Create the Resource Group if it does not exist
+    $resourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
+    if(!$resourceGroup)
+    {
+        Write-Host "Resource group '$ResourceGroupName' does not exist." -ForegroundColor DarkYellow
+        if(!$ResourceGroupLocation) {
+            Write-Host "To create a new resource group, please enter a location." -ForegroundColor DarkYellow
+            $ResourceGroupLocation = Read-Host "ResourceGroupLocation"
+        }
+        Write-Host "Creating resource group '$ResourceGroupName' in location '$ResourceGroupLocation'" -ForegroundColor White
+        New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+        Write-Host "Created resource group '$ResourceGroupName' in location '$ResourceGroupLocation'" -ForegroundColor Green
     }
-    Write-Host "Creating storage account '$StorageAccountName' in location '$ResourceGroupLocation'";
-    New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Location $ResourceGroupLocation -Name $StorageAccountName -SkuName Standard_LRS
-}
-else{
-    Write-Host "Using existing storage account '$StorageAccountName'";
-}
-
-# Define the Azure Storage connection string
-$storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName)[0].Value
-$storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=$StorageAccountName;AccountKey=$storageKey;EndpointSuffix=core.windows.net"
-
-# Create the Function App if it does not exist
-$functionApp = Get-AzureRmResource | Where-Object { $_.ResourceName -eq $FunctionAppName -And $_.ResourceType -eq 'Microsoft.Web/Sites' }
-if(!$functionApp)
-{
-    Write-Host "FunctionApp '$FunctionAppName' does not exist."
-    if (!$ResourceGroupLocation) {
-        Write-Host "To create a new function app, please enter a location.";
-        $ResourceGroupLocation = Read-Host "ResourceGroupLocation";
+    else{
+        Write-Host "Using existing resource group '$ResourceGroupName'" -ForegroundColor White
     }
-    $functionApp = New-AzureRmResource -ResourceType 'Microsoft.Web/Sites' -ResourceName $FunctionAppName -Kind 'functionapp' -Location $ResourceGroupLocation -ResourceGroupName $ResourceGroupName -Properties @{} -Force
+
+    # Create the Storage Account if it does not exist
+    $storageAccount = Get-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
+    if(!$storageAccount)
+    {
+        Write-Host "Storage account '$StorageAccountName' does not exist." -ForegroundColor DarkYellow
+        if(!$ResourceGroupLocation) {
+            Write-Host "To create a new storage account, please enter a location." -ForegroundColor DarkYellow
+            $ResourceGroupLocation = Read-Host "ResourceGroupLocation"
+        }
+        Write-Host "Creating storage account '$StorageAccountName' in location '$ResourceGroupLocation'" -ForegroundColor White
+        New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Location $ResourceGroupLocation -Name $StorageAccountName -SkuName Standard_LRS
+        Write-Host "Created storage account '$StorageAccountName' in location '$ResourceGroupLocation'" -ForegroundColor Green
+    }
+    else{
+        Write-Host "Using existing storage account '$StorageAccountName'" -ForegroundColor White
+    }
+
+    # Define the Azure Storage connection string
+    $storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName)[0].Value
+    $storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=$StorageAccountName;AccountKey=$storageKey;EndpointSuffix=core.windows.net"
+
+    Write-Host "Storage account connection string '$storageConnectionString'" -ForegroundColor White
+
+    # Create the Function App if it does not exist
+    $functionApp = Get-AzureRmResource | Where-Object { $_.ResourceName -eq $FunctionAppName -And $_.ResourceType -eq 'Microsoft.Web/Sites' }
+    if(!$functionApp)
+    {
+        Write-Host "FunctionApp '$FunctionAppName' does not exist." -ForegroundColor White
+        if (!$ResourceGroupLocation) {
+            Write-Host "To create a new function app, please enter a location." -ForegroundColor DarkYellow
+            $ResourceGroupLocation = Read-Host "ResourceGroupLocation"
+        }
+        Write-Host "Creating function app '$FunctionAppName' in location '$ResourceGroupLocation'" -ForegroundColor White
+        $functionApp = New-AzureRmResource -ResourceType 'Microsoft.Web/Sites' -ResourceName $FunctionAppName -Kind 'functionapp' -Location $ResourceGroupLocation -ResourceGroupName $ResourceGroupName -Properties @{} -Force
+        Write-Host "Created function app '$FunctionAppName' in location '$ResourceGroupLocation'" -ForegroundColor Green
+    }
+
+    # Wait 10s for the app to be ready
+    Write-Host "Waiting for the function app '$FunctionAppName' to be ready ..." -ForegroundColor White
+    Start-Sleep -Seconds 10
+
+    # Configure the Function App Settings
+    $appSettings = @{
+        'AllowedTenants' = $AllowedTenants;
+        'AzureWebJobsDashboard' = $storageConnectionString;
+        'AzureWebJobsStorage' = $storageConnectionString;
+        'CLIENT_ID' = $aadApp.ClientId;
+        'CLIENT_SECRET' = $aadApp.ClientSecret;
+        'FUNCTIONS_EXTENSION_VERSION' = "~1";
+        'FUNCTIONS_WORKER_RUNTIME' = "dotnet";
+        'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING' = $storageConnectionString;
+        'WEBSITE_CONTENTSHARE' = $FunctionAppName + "0001";
+        'WEBSITE_NODE_DEFAULT_VERSION' = "8.11.1";
+        'WEBSITE_RUN_FROM_PACKAGE' = "1";
+    }
+
+    # Configure the appSettings
+    Write-Host "Configuring appSettings for the function app" -ForegroundColor White
+    Set-AzureRmWebApp -Name $FunctionAppName -ResourceGroupName $ResourceGroupName -AppSettings $appSettings
+    Write-Host "Configured appSettings for the function app" -ForegroundColor Green
+
+    # Upload the ZIP file of the function and trigger deployment
+    Write-Host "Uploading the source package to the function app" -ForegroundColor White
+    $publishingProfile = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Web/Sites/config' `
+        -ResourceName "$FunctionAppName/publishingcredentials" -Action list -ApiVersion 2015-08-01 -Force
+    $kuduAuthorizationHeader = ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $publishingProfile.properties.publishingUserName, $publishingProfile.properties.publishingPassword))))
+    $kuduZipDeployUrl = "https://$FunctionAppName.scm.azurewebsites.net/api/zipdeploy"
+    $userAgent = "PnP-Modernization/1.0"
+    Invoke-RestMethod -Uri $kuduZipDeployUrl -Headers @{Authorization=$kuduAuthorizationHeader} `
+        -UserAgent $userAgent -Method POST  `
+        -InFile .\sharepointpnpmodernizationeurope.zip `
+        -ContentType "multipart/form-data"
+    Write-Host "Uploaded the source package to the function app" -ForegroundColor Green
+
+    # Configure Authentication/Authorization for the Function App
+    Write-Host "Configuring Authentication settings for the function app" -ForegroundColor White
+    $authResourceName = $FunctionAppName + "/authsettings"
+    $auth = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Web/sites/config' -ResourceName $authResourceName -Action list -ApiVersion 2016-08-01 -Force
+    $auth.properties.enabled = "True"
+    $auth.properties.unauthenticatedClientAction = "RedirectToLoginPage"
+    $auth.properties.tokenStoreEnabled = "True"
+    $auth.properties.defaultProvider = "AzureActiveDirectory"
+    $auth.properties.isAadAutoProvisioned = "False"
+    $auth.properties.clientId = $aadApp.ClientId
+    $auth.properties.clientSecret = $aadApp.ClientSecret
+    $auth.properties.issuer = "https://login.microsoftonline.com/common/"
+
+    New-AzureRmResource -PropertyObject $auth.properties -ResourceGroupName $ResourceGroupName `
+        -ResourceType 'Microsoft.Web/sites/config' -ResourceName $authResourceName `
+        -ApiVersion 2016-08-01 -Force
+
+    Write-Host "Configured Authentication settings for the function app" -ForegroundColor Green
+
+    # Configure CORS
+    Write-Host "Configuring CORS for the function app" -ForegroundColor White
+    $allowedOrigins = @()
+    $allowedOrigins += "*"
+    $functionAppPropertiesObject = @{cors = @{allowedOrigins= $allowedOrigins}}
+    Set-AzureRmResource -PropertyObject $functionAppPropertiesObject -ResourceGroupName $ResourceGroupName `
+        -ResourceType 'Microsoft.Web/sites/config' -ResourceName "$FunctionAppName/web" `
+        -ApiVersion 2015-08-01 -Force
+    Write-Host "Configured CORS for the function app" -ForegroundColor Green
+
+    Write-Host "Process completed!" -ForegroundColor Green
 }
-
-# Wait 10s for the app to be ready
-Start-Sleep -Seconds 10
-
-# Configure the Function App Settings
-$appSettings = @{
-    'AllowedTenants' = $AllowedTenants;
-    'AzureWebJobsDashboard' = $storageConnectionString;
-    'AzureWebJobsStorage' = $storageConnectionString;
-    'CLIENT_ID' = $aadApp.ClientId;
-    'CLIENT_SECRET' = $aadApp.ClientSecret;
-    'FUNCTIONS_EXTENSION_VERSION' = "~1";
-    'FUNCTIONS_WORKER_RUNTIME' = "dotnet";
-    'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING' = $storageConnectionString;
-    'WEBSITE_CONTENTSHARE' = $FunctionAppName + "0001";
-    'WEBSITE_NODE_DEFAULT_VERSION' = "8.11.1";
-    'WEBSITE_RUN_FROM_PACKAGE' = "1";
-}
-
-# Force NewtonSoft.Json installation
-# $module = import-module newtonsoft.json -ErrorAction SilentlyContinue -PassThru
-# if(!$module)
-# {
-#     Write-Output "installing newtonsoft.json"
-#     Install-Package Newtonsoft.Json -Force
-#     import-module newtonsoft.json
-# }
-Set-AzureRmWebApp -Name $FunctionAppName -ResourceGroupName $ResourceGroupName -AppSettings $appSettings
-
-# Upload the ZIP file of the function and trigger deployment
-$publishingProfile = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Web/Sites/config' `
-    -ResourceName "$FunctionAppName/publishingcredentials" -Action list -ApiVersion 2015-08-01 -Force
-$kuduAuthorizationHeader = ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $publishingProfile.properties.publishingUserName, $publishingProfile.properties.publishingPassword))))
-$kuduZipDeployUrl = "https://$FunctionAppName.scm.azurewebsites.net/api/zipdeploy"
-$userAgent = "PnP-Modernization/1.0"
-Invoke-RestMethod -Uri $kuduZipDeployUrl -Headers @{Authorization=$kuduAuthorizationHeader} `
-    -UserAgent $userAgent -Method POST  `
-    -InFile .\sharepointpnpmodernizationeurope.zip `
-    -ContentType "multipart/form-data"
-
-# Configure Authentication/Authorization for the Function App
-$authResourceName = $FunctionAppName + "/authsettings"
-$auth = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Web/sites/config' -ResourceName $authResourceName -Action list -ApiVersion 2016-08-01 -Force
-$auth.properties.enabled = "True"
-$auth.properties.unauthenticatedClientAction = "RedirectToLoginPage"
-$auth.properties.tokenStoreEnabled = "True"
-$auth.properties.defaultProvider = "AzureActiveDirectory"
-$auth.properties.isAadAutoProvisioned = "False"
-$auth.properties.clientId = $aadApp.ClientId
-$auth.properties.clientSecret = $aadApp.ClientSecret
-$auth.properties.issuer = "https://login.microsoftonline.com/common/"
-
-New-AzureRmResource -PropertyObject $auth.properties -ResourceGroupName $ResourceGroupName `
-    -ResourceType 'Microsoft.Web/sites/config' -ResourceName $authResourceName `
-    -ApiVersion 2016-08-01 -Force
-
-# Configure CORS
-$allowedOrigins = @()
-$allowedOrigins += "*"
-$functionAppPropertiesObject = @{cors = @{allowedOrigins= $allowedOrigins}}
-Set-AzureRmResource -PropertyObject $functionAppPropertiesObject -ResourceGroupName $ResourceGroupName `
-    -ResourceType 'Microsoft.Web/sites/config' -ResourceName "$FunctionAppName/web" `
-    -ApiVersion 2015-08-01 -Force
