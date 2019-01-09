ddd7b8c48288371612c41f31df3ff5424f63fe79
Bert Jansen <bjansen@microsoft.com>
2019-01-09 14:16:28 +0100

    Prep for 2019 releases

--- Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Properties/AssemblyInfo.cs
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SharePointPnP.Modernization.Framework")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1812.2")]
-[assembly: AssemblyFileVersion("1.0.1812.2")]
+[assembly: AssemblyVersion("1.0.1901.0")]
+[assembly: AssemblyFileVersion("1.0.1901.0")]
 
 [assembly: InternalsVisibleTo("SharePointPnP.Modernization.Framework.Tests")]
