41f5787ec2df185b387a824aedb7ce9e54392841
Paul Bullock <paul.bullock@capacreative.co.uk>
2019-02-20 09:12:34 +0000

    Added switch to acitvate feature

--- Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Transform/PageTransformationInformation.cs
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Transform/PageTransformationInformation.cs
@@ -45,6 +45,7 @@ namespace SharePointPnP.Modernization.Framework.Transform
             SkipTelemetry = false;
             SetDefaultTargetPagePrefix();
             SetDefaultSourcePagePrefix();
+            IncludeReferencedAssets = false; // Default to false for now until out of experimental phase
         }
         #endregion
 
@@ -98,6 +99,11 @@ namespace SharePointPnP.Modernization.Framework.Transform
         /// Configuration driven by the presence of a modernization center
         /// </summary>
         public ModernizationCenterInformation ModernizationCenterInformation { get; set; }
+
+        /// <summary>
+        /// Includes referenced assets in the web part
+        /// </summary>
+        public bool IncludeReferencedAssets { get; set; }
         #endregion
 
         #region Webpart replacement configuration
