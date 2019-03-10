7dac3ffc57179f22f6e0155428d11ac2dc0e857a
Bert Jansen <bjansen@microsoft.com>
2019-03-10 15:42:11 +0100

    first draft of pagelayout schema

---
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Publishing/PageLayoutManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SharePointPnP.Modernization.Framework.Publishing
+{
+    /// <summary>
+    /// Class used to manage SharePoint Publishing page layouts
+    /// </summary>
+    public class PageLayoutManager
+    {
+        private ClientContext sourceContext;
+
+        #region Construction
+        public PageLayoutManager(ClientContext context)
+        {
+            this.sourceContext = context;
+        }
+        #endregion
+
+        public PublishingPageTransformation ReadPageLayoutMappingFile(string pageLayoutMappingFile)
+        {
+            XmlSerializer xmlMapping = new XmlSerializer(typeof(PublishingPageTransformation));
+            using (var stream = new FileStream(pageLayoutMappingFile, FileMode.Open))
+            {
+                return (PublishingPageTransformation)xmlMapping.Deserialize(stream);
+            }
+        }
+
+    }
+}
