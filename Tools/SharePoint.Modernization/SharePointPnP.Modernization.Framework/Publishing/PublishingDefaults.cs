4fbb89e64218253b2e7b8a1caeec4dc11913d72c
Paul Bullock <paul.bullock@capacreative.co.uk>
2019-03-20 16:58:14 +0000

    Added placeholder default class

---
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Publishing/PublishingDefaults.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.Modernization.Framework.Publishing
+{
+    /// <summary>
+    /// Contains a central point for defaults for publishing page processing
+    /// </summary>
+    public class PublishingDefaults
+    {
+        //Web Part Zone Row/Columns
+
+
+        //Field Control Defaults
+
+
+    }
+}
