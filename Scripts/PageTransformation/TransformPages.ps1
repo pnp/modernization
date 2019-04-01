6330dcec2822566d45460ef0e23f3d258346f397
Bert Jansen <jansenbe@users.noreply.github.com>
2019-04-01 13:22:05 +0200

    Small  update to Write-Host cmdlet calls

--- Scripts/PageTransformation/TransformPages.ps1
+++ Scripts/PageTransformation/TransformPages.ps1
@@ -24,12 +24,12 @@ foreach($page in $pages)
         # No need to convert modern pages again
         if ($page.FieldValues["ClientSideApplicationId"] -eq "b6917cb1-93a0-4b97-a84d-7cf49975d4ec" ) 
         { 
-            Write-Host `Page $page.FieldValues["FileLeafRef"] is modern, no need to modernize it again`
+            Write-Host "Page " $page.FieldValues["FileLeafRef"] " is modern, no need to modernize it again"
         } 
         else 
         { 
             # Create a modern version of this page
-            Write-Host `Modernizing $page.FieldValues["FileLeafRef"]...`
+            Write-Host "Modernizing " $page.FieldValues["FileLeafRef"] "..."
             $modernPage = ConvertTo-PnPClientSidePage -Identity $page.FieldValues["FileLeafRef"] -Overwrite
             Write-Host "Done" -ForegroundColor Green
         }
