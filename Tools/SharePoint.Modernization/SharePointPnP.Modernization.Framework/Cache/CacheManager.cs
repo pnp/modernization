c1c674f65e627ccbfcc84c7a1feb412d5c4d3f74
Bert Jansen <bjansen@microsoft.com>
2019-05-03 15:52:46 +0200

    Cleanup of old method implementation

--- Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Cache/CacheManager.cs
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Cache/CacheManager.cs
@@ -569,17 +569,8 @@ namespace SharePointPnP.Modernization.Framework.Cache
         {
             SHA256CryptoServiceProvider provider = new SHA256CryptoServiceProvider();
             byte[] hash = provider.ComputeHash(Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(randomString)));
-            string componentKeyToCache = BitConverter.ToString(hash);
+            string componentKeyToCache = BitConverter.ToString(hash).Replace("-", "");
             return componentKeyToCache;
-
-            //var crypt = new System.Security.Cryptography.SHA256Managed();
-            //var hash = new StringBuilder();
-            //byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
-            //foreach (byte theByte in crypto)
-            //{
-            //    hash.Append(theByte.ToString("x2"));
-            //}
-            //return hash.ToString();
         }
 
         private void AddFieldRef(OfficeDevPnP.Core.Framework.Provisioning.Model.FieldRefCollection fieldRefs, Guid Id, string name)
