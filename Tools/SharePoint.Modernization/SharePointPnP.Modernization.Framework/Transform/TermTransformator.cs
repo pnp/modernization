3825775697f656048cb71466378056e346e0e574
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-02-03 09:12:44 +0000

    Added base class for term store mapping

---
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Transform/TermTransformator.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.SharePoint.Client;
+using SharePointPnP.Modernization.Framework.Telemetry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.Modernization.Framework.Transform
+{
+    public  class TermTransformator : BaseTransform
+    {
+        private ClientContext _sourceContext;
+        private ClientContext _targetContext;
+
+        #region Construction        
+
+        /// <summary>
+        /// Constructor for the Term Transformator class
+        /// </summary>
+        /// <param name="baseTransformationInformation"></param>
+        /// <param name="sourceContext"></param>
+        /// <param name="targetContext"></param>
+        /// <param name="logObservers"></param>
+        public TermTransformator(BaseTransformationInformation baseTransformationInformation, ClientContext sourceContext, ClientContext targetContext, IList<ILogObserver> logObservers = null)
+        {
+            // Hookup logging
+            if (logObservers != null)
+            {
+                foreach (var observer in logObservers)
+                {
+                    base.RegisterObserver(observer);
+                }
+            }
+
+            // Ensure source and target context are set
+            if (sourceContext == null && targetContext != null)
+            {
+                sourceContext = targetContext;
+            }
+
+            if (targetContext == null && sourceContext != null)
+            {
+                targetContext = sourceContext;
+            }
+
+
+            this._sourceContext = sourceContext;
+            this._targetContext = targetContext;
+
+        }
+
+        #endregion
+
+
+    }
+}
