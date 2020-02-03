34d0aba6402052e332240cd8e1ee7c2f883db05f
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-02-03 09:43:38 +0000

    Added Term Mapping File Tests

---
+++ Tools/SharePoint.Modernization/SharePointPnP.Modernization.Framework.Tests/Transform/Mapping/TermMappingTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharePointPnP.Modernization.Framework.Utilities;
+
+namespace SharePointPnP.Modernization.Framework.Tests.Transform.Mapping
+{
+    [TestClass]
+    public class TermMappingTests
+    {
+        [TestMethod]
+        public void TermMappingFileLoadTest()
+        {
+            FileManager fm = new FileManager();
+            var mapping = fm.LoadTermMappingFile(@"..\..\Transform\Mapping\term_mapping_sample.csv");
+
+            Assert.IsTrue(mapping.Count > 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TermMappingFileNotFoundTest()
+        {
+            FileManager fm = new FileManager();
+            var mapping = fm.LoadTermMappingFile(@"..\..\Transform\Mapping\idontexist_sample.csv");
+        }
+
+    }
+}
